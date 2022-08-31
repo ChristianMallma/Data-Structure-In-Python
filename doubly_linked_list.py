b5f7ff24cb0b1a0a21fc475ae61346fc96b7cf68
Christian <christian.mallma@fidecorp.com>
2022-08-30 21:39:45 -0500

    add doubly_linked_list

---
+++ doubly_linked_list.py
@@ -0,0 +1,25 @@
+class Node:
+  def __init__(self, value) -> None:
+    self.value = value
+    self.next_node = None
+    self.previous_node = None
+
+# Creating nodes
+node1 = Node("one")
+node2 = Node("two")
+node3 = Node("three")
+
+# Linking
+node1.next_node = node2
+
+node2.previous_node = node1
+node2.next_node = node3
+
+node3.previous_node = node2
+
+# Test
+print(node1.value)
+print(node2.previous_node.value)
+
+print(node2.next_node.value)
+print(node3.value)
